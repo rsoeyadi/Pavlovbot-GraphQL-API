05f9d6bb5d5f414ea39112102a94b9f8ef56c583
Ryan Soeyadi <rsoeyadi@Ryans-MBP.verizon.net>
2024-11-12 17:29:09 -0500

    added first resolver w pg library

--- server.ts
+++ server.ts
@@ -4,6 +4,15 @@ import { startStandaloneServer } from "@apollo/server/standalone"
 import { readFileSync } from "fs";
 import path from "path";
 import { gql } from "graphql-tag";
+import pg from 'pg';
+
+// create a connection pool for users
+const { Pool } = pg;
+const config = {
+  connectionString: process.env.DATABASE_STRING,
+}
+
+const pool = new Pool(config);
 
 // readFileSync is just a Node way of reading the contents of the specified file
 // I am passing it into the gql tag function so it's understood by Apollo
@@ -13,13 +22,31 @@ const typeDefs = gql(
   })
 );
 
+const resolvers = {
+  Query: {
+    getMostRecentPrompt: async (): Promise<string> => {
+      try { // pool deals with connecting/releasing implicitly
+        const result = await pool.query('SELECT * from prompts')
+        return result.rows[0].prompt_text;
+      } catch {
+        return "Failed to query database"
+      }
+    }
+  }
+}
 // start the server w the schema
 async function startApolloServer(): Promise<void> {
-  const server = new ApolloServer({ typeDefs });
-  const { url } = await startStandaloneServer(server);
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+  });
   console.log(`
     The server is up running at ${url}
   `)
 }
+
 
 startApolloServer()

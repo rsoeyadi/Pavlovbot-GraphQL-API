7a7040f199c8f2c9f328c021d5a2539d89538a21
Ryan Soeyadi <rsoeyadi@Ryans-MBP.verizon.net>
2024-11-12 18:44:26 -0500

    fixed typing for getAllPrompts query

--- server.ts
+++ server.ts
@@ -22,6 +22,14 @@ const typeDefs = gql(
   })
 );
 
+export type Prompt = {
+  id: number,
+  prompt_text: string,
+  created_at: string,
+  is_active: boolean,
+  likes_counter: number,
+}
+
 const resolvers = {
   Query: {
     getMostRecentPrompt: async (): Promise<string> => {
@@ -31,9 +39,20 @@ const resolvers = {
       } catch {
         return "Failed to query database"
       }
+    },
+    getAllPrompts: async (): Promise<Prompt[]> => {
+      try {
+        const result = await pool.query<Prompt>('SELECT * from prompts');
+        return result.rows
+      } catch {
+        return []
+      }
     }
+
+
   }
 }
+
 // start the server w the schema
 async function startApolloServer(): Promise<void> {
   const server = new ApolloServer({

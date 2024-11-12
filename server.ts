7dd7e7b3333a0cee3cc81f7e865c637c4839a25b
Ryan Soeyadi <rsoeyadi@Ryans-MBP.verizon.net>
2024-11-11 20:37:54 -0500

    added a schema for apollo server

--- server.ts
+++ server.ts
@@ -1,10 +1,25 @@
-import express from 'express'
 import 'dotenv/config'
+import { ApolloServer } from '@apollo/server'
+import { startStandaloneServer } from "@apollo/server/standalone"
+import { readFileSync } from "fs";
+import path from "path";
+import { gql } from "graphql-tag";
 
-// express setup
-const app = express()
-const port = 3000
+// readFileSync is just a Node way of reading the contents of the specified file
+// I am passing it into the gql tag function so it's understood by Apollo
+const typeDefs = gql(
+  readFileSync(path.resolve(__dirname, "./schema.graphql"), {
+    encoding: "utf-8",
+  })
+);
 
-app.listen(port, () => {
-  console.log("Running on port 3000")
-})
+// start the server w the schema
+async function startApolloServer(): Promise<void> {
+  const server = new ApolloServer({ typeDefs });
+  const { url } = await startStandaloneServer(server);
+  console.log(`
+    The server is up running at ${url}
+  `)
+}
+
+startApolloServer()
